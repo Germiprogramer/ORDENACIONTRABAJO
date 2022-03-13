d6f9419ba8ebd714790b520f6a7cce9f8527e8b9
Germán Llorente <91720991+Germiprogramer@users.noreply.github.com>
2022-03-13 11:19:49 +0000

    ejercicio 3 mas o menos

--- Classes/completarlasespecificaciones.py
+++ Classes/completarlasespecificaciones.py
@@ -1,7 +1,16 @@
 class Completarlasespecificaciones:
   def __init__(self, tabla):
     self.tabla = list(tabla)
-  def elegirsegmento():
-    max = input(int("elige el límite menor: "))
-    min = input(int("elige el límite mayor"))
-  def sacarmaximo(self, tabla, )
+  
+  def elegirsegmento(self, max, min):
+    max = int(input("elige el límite menor: "))
+    min = int(input("elige el límite mayor: "))
+    return max, min
+  
+  def sacarmaximo(self, tabla, max, min):
+    for i in range(min, max):
+        maximo = tabla[i] 
+        if tabla[i] > maximo:
+            maximo = tabla[i]
+    return maximo
+  
---
+++ main.py
@@ -0,0 +1,22 @@
+if __name__ == "__main__":
+  from dicotomia import ordenacion
+  print("Ejercicio 1")
+  tabla = [1,2,5,3,2,1,4,7]
+  tablaordenada = ordenacion(tabla)
+  print(tablaordenada)
+
+  print("Ejercicio 2")
+  from ordenartareas import ordenar
+
+  print("Ejercicio 3")
+  from entero import MAX, MIN
+  from completarlasespecificaciones import elegirsegmento, sacarmaximo
+  tabla = [1,2,5,3,2,1,4,7]
+  max = MAX
+  min = MIN
+
+  elegirsegmento(self, max, min)
+  sacarmaximo(self, tabla, max, min)
+  print("El máximo es {}".format(maximo))
+
+
